2bc259410e2abb99aefec3effb2409d580e8cfc1
realjck <jckleinbourg@gmail.com>
2024-07-18 10:26:24 +0200

    Ajoute modèle YoutubeVideo, DAO et dépendances

---
+++ app/src/main/java/com/jck/mybestyoutube/model/YoutubeVideo.java
@@ -0,0 +1,80 @@
+package com.jck.mybestyoutube.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class YoutubeVideo {
+
+    @PrimaryKey(autoGenerate = true)
+    private Long id;
+    @ColumnInfo(name = "titre")
+    private String titre;
+    @ColumnInfo(name = "description")
+    private String description;
+    @ColumnInfo(name = "url")
+    private String url;
+    @ColumnInfo(name = "categorie")
+    private String categorie;
+    @ColumnInfo(name = "favori")
+    private Boolean favori;
+
+    public YoutubeVideo(String titre, String description, String url, String categorie, Boolean favori) {
+        this.titre = titre;
+        this.description = description;
+        this.url = url;
+        this.categorie = categorie;
+        this.favori = favori;
+    }
+    public YoutubeVideo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public Boolean getFavori() {
+        return favori;
+    }
+
+    public void setFavori(Boolean favori) {
+        this.favori = favori;
+    }
+}
---
+++ app/src/main/java/com/jck/mybestyoutube/persistence/YoutubeVideoDAO.java
@@ -0,0 +1,29 @@
+package com.jck.mybestyoutube.persistence;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.jck.mybestyoutube.model.YoutubeVideo;
+
+import java.util.List;
+
+public interface YoutubeVideoDAO {
+
+    @Query("SELECT * FROM youtubevideo WHERE id = :id")
+    public YoutubeVideo find(Long id);
+
+    @Query("SELECT * FROM youtubevideo")
+    public List<YoutubeVideo> list();
+
+    @Insert
+    public void add(YoutubeVideo... youtubeVideos);
+
+    @Update
+    public void update(YoutubeVideo... youtubeVideos);
+
+    @Delete
+    public void delete(YoutubeVideo... youtubeVideos);
+
+}
